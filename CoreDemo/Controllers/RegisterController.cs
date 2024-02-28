7374a1751cd6da552b26a5374e49a7b6efba5c4e
Berk Erdem <127400459+berdem98@users.noreply.github.com>
2024-02-28 18:29:13 +0300

    Add project files.

---
+++ CoreDemo/Controllers/RegisterController.cs
@@ -0,0 +1,42 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+	public class RegisterController : Controller
+	{
+		WriterManager wm= new WriterManager(new EfWriterRepository());
+		//****Ekleme işlemi yapılırken, httpget ve httppost attributelerinin tanımlandığı metotların isimleri aynı olmak zorundadır!****
+		//HttpGet sayfa çalışınca çalışır, HttpPost Buton tetiklenince çalışır!!
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Index(Writer p)
+		{
+			WriterValidator wv = new WriterValidator();
+			ValidationResult results = wv.Validate(p);
+			if (results.IsValid)
+			{
+				p.WriterStatus = true;
+				p.WriterAbout = "Deneme Test";
+				wm.WriterAdd(p);
+				return RedirectToAction("Index", "Blog");
+			}
+			else
+			{
+				foreach (var item in results.Errors)
+				{
+					ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+				}
+			}
+			return View();
+		}
+	}
+}
---
+++ DataAccessLayer/Migrations/20240128124349_LastMig.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class LastMig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "BlogContent",
+                table: "Blogs",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "BlogContent",
+                table: "Blogs",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
