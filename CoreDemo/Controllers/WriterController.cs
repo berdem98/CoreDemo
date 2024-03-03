66a48b42823b9b8a441c3f0ab38b0fe4a83a8aaa
Berk Erdem <127400459+berdem98@users.noreply.github.com>
2024-03-03 17:14:35 +0300

    Writer Dashboard

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -23,5 +23,15 @@ namespace CoreDemo.Controllers
 		{
 			return View();
 		}
+		[AllowAnonymous]
+		public PartialViewResult WriterNavbarPartial()
+		{
+			return PartialView();
+		}
+        [AllowAnonymous]
+        public PartialViewResult WriterFooterPartial()
+		{
+			return PartialView();
+		}
 	}
 }
